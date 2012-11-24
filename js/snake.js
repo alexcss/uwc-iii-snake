e990a48c0da77d9f3306e9e3bf695852f1a7f4b0
Alexey <kalyuzhnyi@gmail.com>
2012-11-24 17:03:15 +0200

    snake update

--- js/snake.js
+++ js/snake.js
@@ -7,7 +7,7 @@
 		var gameOver = false;
 		var score = 0;
 		var snakeArr, food;
-		var cw = 20; //size of snake bit
+		var sc = 20; // Size of snake cell
 		var direction = 'right';
 
 		var $canvas = $('#game-field');
@@ -16,49 +16,47 @@
 
 		var w = $canvas.width();
 		var h = $canvas.height();
-
+		
+		// Set snake
 		var setSnake = function () {
-			var length = 5; //Length of the snake
+			var length = 4; //Length of the snake
 			snakeArr = [];
 			for (var i = length - 1; i >= 0; i--) {
-				//This will create a horizontal snake starting from the top left
 				snakeArr.push({
 					x: i,
 					y: 0
 				});
 			}
-			changeDir = false;
 		}
-
-		//Lets create the food now
+		
+		// Set food coordinates
 		var setFood = function () {
 			food = {
-				x: Math.round(Math.random() * (w - cw) / cw),
-				y: Math.round(Math.random() * (h - cw) / cw),
+				x: Math.round(Math.random() * (w - sc) / sc),
+				y: Math.round(Math.random() * (h - sc) / sc),
 			};
-			//This will create a cell with x/y between 0-44
-			//Because there are 45(450/10) positions accross the rows and columns
 		}
-
+		
+		// Draw cell on game field
 		var drawCell = function (x, y, color) {
 			ctx.fillStyle = color;
-			ctx.fillRect(x * cw, y * cw, cw, cw);
+			ctx.fillRect(x * sc, y * sc, sc, sc);
 			ctx.strokeStyle = "white";
-			ctx.strokeRect(x * cw, y * cw, cw, cw);
+			ctx.strokeRect(x * sc, y * sc, sc, sc);
 		}
+		// Check Collision
 		var checkCollision = function (nx, ny, arr) {
-
 			var snakeLoop = function (x, y, arr) {
 				for (var i = 0; i < arr.length; i++) {
 					if (arr[i].x == x && arr[i].y == y) return true;
 				}
 				return false;
 			}
-			if (nx == -1 || nx == w / cw || ny == -1 || ny == h / cw || snakeLoop(nx, ny, arr)) {
+			if (nx == -1 || nx == w / sc || ny == -1 || ny == h / sc || snakeLoop(nx, ny, arr)) {
 				gameOver = true;
 			}
 		}
-
+		// Change snake direction by pressing key
 		var changeDirection = function (key) {
 			if (key == 37 && direction != "right") {
 				direction = "left";
@@ -70,7 +68,7 @@
 				direction = "down";
 			};
 		};
-
+		// Show Game Over Message
 		var gameOverMsg = function () {
 			ctx.font = 'bold 30px sans-serif';
 			ctx.fillStyle = '#333';
@@ -80,13 +78,11 @@
 			ctx.lineWidth = 2;
 			var centreX = w / 2;
 			var centreY = h / 2;
-			ctx.strokeText('Игра окончена', centreX, centreY - 10);
-			ctx.fillText('Игра окончена', centreX, centreY - 10);
-		
+			ctx.fillText('Игра окончена', centreX, centreY - 10);		
 			ctx.font = 'bold 14px sans-serif';
-	    	ctx.strokeText('Нажмите кнопку "ЗАНОВО" для новой игры', centreX, centreY + 15);
 	    	ctx.fillText('Нажмите кнопку "ЗАНОВО" для новой игры', centreX, centreY + 15);			
 		}
+		// Draw game process
 		var drawGame = function () {
 
 			ctx.fillStyle = "white";
@@ -118,20 +114,18 @@
 					y: ny
 				};
 				score++;
-				speed -= 10;
+				speed -= 20;
 				$('.score i').text(score);
-				//Create new food
 				setFood();
 			} else {
-				var tail = snakeArr.pop(); //pops out the last cell
+				var tail = snakeArr.pop();
 				tail.x = nx;
 				tail.y = ny;
 			}
 
-			snakeArr.unshift(tail); //puts back the tail as the first cell
+			snakeArr.unshift(tail); 
 			for (var i = 0; i < snakeArr.length; i++) {
 				var c = snakeArr[i];
-				//Lets paint 10px wide cells
 				drawCell(c.x, c.y, "blue");
 			}
 			drawCell(food.x, food.y, "red");
@@ -144,7 +138,7 @@
 		var initGame = function () {
 			setSnake();
 			setFood();
-			drawGame();
+			//drawGame();
 			gameLoop();
 		}
 
@@ -154,7 +148,7 @@
 				$('#restart').fadeToggle(200);
 				clearInterval(timeout);
 				gameOverMsg();
-			} else timeout = setTimeout(gameLoop, speed); //do it all again
+			} else timeout = setTimeout(gameLoop, speed);
 		}
 		initGame();
 	};
@@ -193,6 +187,5 @@
 	$('#restart').click(function () {
 		restartGame()
 	});
-
 
 })(jQuery);
